cf36664488160f817895314ae034a05f5b45719c
penyz <397610336@qq.com>
2024-09-16 22:36:24 +0800

    case_648：调整动态规划算法方案

--- case_648/case_648_test.go
+++ case_648/case_648_test.go
@@ -32,34 +32,28 @@ func TestHashRing_Balance(t *testing.T) {
 			name: "原在A节点，调整后还在A节点",
 			uid:  100,
 			before: func() *HashRing {
-				h := NewHashRing(nodes, DefaultHashRingSlotNum)
+				h := NewHashRing(nodes, DefaultHashRingSlotNum, func(req any) int {
+					return req.(int) % DefaultHashRingSlotNum
+				})
 				return h
 			},
 			wantRes: "节点a缓存",
 		},
 		{
-			name: "原在A节点，调整后在B节点",
-			uid:  100,
+			name: "原在B节点，调整后在A节点",
+			uid:  4,
 			before: func() *HashRing {
-				h := NewHashRing(nodes, DefaultHashRingSlotNum)
-				uid := 2
-				for i := 0; i < 6999; i++ {
-					n := h.GetNode(uid)
-					n.GetCache(uid)
-				}
-				uid = 600
-				for i := 0; i < 2000; i++ {
-					n := h.GetNode(uid)
-					n.GetCache(uid)
-				}
-				uid = 1000
-				for i := 0; i < 1000; i++ {
-					n := h.GetNode(uid)
-					n.GetCache(uid)
-				}
+				h := NewHashRing(nodes, 10, func(req any) int {
+					return req.(int) % 10
+				})
+
+				h.SetRequestNumOfSlot([]int{
+					10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+				})
+
 				return h
 			},
-			wantRes: "节点b缓存",
+			wantRes: "节点a缓存",
 		},
 	}
 
@@ -69,7 +63,9 @@ func TestHashRing_Balance(t *testing.T) {
 			n := h.GetNode(tc.uid)
 			_, err := n.GetCache(tc.uid)
 			require.NoError(t, err)
+
 			h.Balance()
+
 			n = h.GetNode(tc.uid)
 			c, err := n.GetCache(tc.uid)
 			require.NoError(t, err)
