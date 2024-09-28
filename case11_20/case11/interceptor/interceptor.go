fb676f1b490f4daf1be8bb7a6c96477fbc0c3710
Deng Ming <ming_flycash@qq.com>
2024-09-28 18:44:31 +0800

    修复 case11

--- case11_20/case11/interceptor/interceptor.go
+++ case11_20/case11/interceptor/interceptor.go
@@ -6,12 +6,10 @@ import (
 )
 
 func UnaryServerInterceptor(tb *TokenBucket) grpc.UnaryServerInterceptor {
-
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		if !tb.Consume(1) {
 			ctx = context.WithValue(ctx, "RateLimited", true)
 		}
-
 		// 继续处理请求
 		return handler(ctx, req)
 	}
